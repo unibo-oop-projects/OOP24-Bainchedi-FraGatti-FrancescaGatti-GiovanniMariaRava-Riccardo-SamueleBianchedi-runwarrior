d1ff5cba65e05b6c785a9707b80fa8b2a7614592
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-04-13 19:34:46 +0200

    created Main class that creates the main frame and starts the game

---
+++ src/main/java/it/unibo/runwarrior/view/RunwarriorMain.java
@@ -0,0 +1,26 @@
+package it.unibo.runwarrior.view;
+
+import javax.swing.JFrame;
+
+public class RunwarriorMain{
+
+    private JFrame mainFrame;
+    private GameLoopPanel glp;
+
+    public RunwarriorMain(){
+        mainFrame = new JFrame("runwarrior");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLocationByPlatform(true);
+        mainFrame.setResizable(true);
+
+        glp = new GameLoopPanel();
+        glp.startGame();
+
+        mainFrame.add(glp);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+    public static void main(String[] args){
+        new RunwarriorMain();
+    }
+}
