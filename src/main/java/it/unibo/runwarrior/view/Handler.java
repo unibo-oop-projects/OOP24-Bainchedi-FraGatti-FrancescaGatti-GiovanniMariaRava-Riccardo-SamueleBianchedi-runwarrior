9c6ccea87109af12584d37332f88c5f1f7bccb0f
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-14 18:39:53 +0200

    created Handler class

---
+++ src/main/java/it/unibo/runwarrior/view/Handler.java
@@ -0,0 +1,11 @@
+package it.unibo.runwarrior.view;
+
+import java.util.LinkedList;
+
+import it.unibo.runwarrior.model.EnemyImpl;
+
+public class Handler {
+    public LinkedList<EnemyImpl> enemies = new LinkedList<>();
+    
+
+}
