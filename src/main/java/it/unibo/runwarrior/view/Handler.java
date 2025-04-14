c9d9534a9afa215fffc0374f3e493e9ac4812af9
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-14 18:42:24 +0200

    made render and update in Handler class

--- src/main/java/it/unibo/runwarrior/view/Handler.java
+++ src/main/java/it/unibo/runwarrior/view/Handler.java
@@ -1,11 +1,21 @@
 package it.unibo.runwarrior.view;
 
-import java.util.LinkedList;
+import java.awt.Graphics;
+import java.util.*;
 
 import it.unibo.runwarrior.model.EnemyImpl;
 
 public class Handler {
     public LinkedList<EnemyImpl> enemies = new LinkedList<>();
+
+    public void render(Graphics g){
+        for (EnemyImpl enemy : enemies) enemy.render(g);
+    }
+
+    public void update(){
+        for (EnemyImpl enemy : enemies) enemy.update();
+    }
+
     
 
 }
