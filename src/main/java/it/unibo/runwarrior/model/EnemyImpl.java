6754afe2e6e80554f925fae86079e514e0e2225e
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-14 18:35:44 +0200

    created getters and setters for EnemyImpl

--- src/main/java/it/unibo/runwarrior/model/EnemyImpl.java
+++ src/main/java/it/unibo/runwarrior/model/EnemyImpl.java
@@ -40,6 +40,54 @@ public abstract class EnemyImpl implements Enemy{
         // TODO Auto-generated method stub
         
     }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public boolean isSolid() {
+        return solid;
+    }
+    public void setSolid(boolean solid) {
+        this.solid = solid;
+    }
+    public int getVelocityX() {
+        return velocityX;
+    }
+    public void setVelocityX(int velocityX) {
+        this.velocityX = velocityX;
+    }
+    public int getVelocityY() {
+        return velocityY;
+    }
+    public void setVelocityY(int velocityY) {
+        this.velocityY = velocityY;
+    }
+    public BufferedImage getImage() {
+        return image;
+    }
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 
 
 }
