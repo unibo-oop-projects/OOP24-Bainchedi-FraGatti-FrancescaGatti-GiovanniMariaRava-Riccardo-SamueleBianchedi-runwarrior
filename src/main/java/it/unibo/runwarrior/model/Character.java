dcccfc17d6d9b30afc3c073808078f8cd76bfb38
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-04-19 17:09:54 +0200

    created Character interface

---
+++ src/main/java/it/unibo/runwarrior/model/Character.java
@@ -0,0 +1,16 @@
+package it.unibo.runwarrior.model;
+
+import java.awt.Graphics2D;
+
+public interface Character {
+
+    public void update();
+    public void drawPlayer(Graphics2D gr2);
+    public void playerImage();
+    public void setLocationAfterPowerup(int x, int y, int realx);
+
+    public int getPlX();
+    public int getPlY();
+    public int getScX();
+    public int getScY();
+}
