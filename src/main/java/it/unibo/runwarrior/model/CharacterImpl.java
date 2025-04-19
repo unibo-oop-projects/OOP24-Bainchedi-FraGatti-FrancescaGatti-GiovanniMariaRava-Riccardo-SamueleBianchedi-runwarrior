13b96b45c9f93d7bf59262f798d3b49a3fef01a5
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-04-19 17:52:36 +0200

    implemented player movement, right and left

--- src/main/java/it/unibo/runwarrior/model/CharacterImpl.java
+++ src/main/java/it/unibo/runwarrior/model/CharacterImpl.java
@@ -2,6 +2,7 @@ package it.unibo.runwarrior.model;
 
 import java.awt.Graphics2D;
 
+import it.unibo.runwarrior.controller.CharacterComand;
 import it.unibo.runwarrior.view.GameLoopPanel;
 
 public class CharacterImpl implements Character{
@@ -10,14 +11,20 @@ public class CharacterImpl implements Character{
     public static final int START_Y = 387;
     public static final int START_X = 320;
     public static final int maxScreenX = 550;//x IN CUI SI FERMA IL PLAYER NELLO SCHERMO
-    public static final int minScreenX = 180;//y IN CUI SI FERMA IL PLAYER NELLO SCHERMO
+    public int minScreenX = 180;//y IN CUI SI FERMA IL PLAYER NELLO SCHERMO (se raggiunge l'inizio mappa diventa 0)
 
     private int playerX = START_X;//POSIZIONE ORIZZONTALE DEL PLAYER NELLA MAPPA
     private int playerY = START_Y;// * VERTICALE
     private int screenX = START_X;//POSIZIONE ORIZZONTALE DEL PLAYER NELLO SCHERMO
     private int screenY = START_Y;// * VERITCALE (NON USATA PERCHè LA POSIZIONE VERTICALE è DATA SOLO DAL SALTO)
+    private boolean rightDirection;
+    private int speed = 5;
+    private int speedJumpUP = 13; 
+    private int speedJumpDown = 8;
+    private int groundX = 0;//variabile che permette lo scorrimento della mappa
 
     public GameLoopPanel glp;
+    public CharacterComand cmd;
 
     public CharacterImpl(GameLoopPanel panel){
         this.glp = panel;
@@ -25,6 +32,29 @@ public class CharacterImpl implements Character{
 
     @Override
     public void update() {
+        if(cmd.getRight()){
+            rightDirection = true;
+            playerX += speed;
+            if(screenX < maxScreenX){
+                screenX += speed;
+            }
+            if(screenX == maxScreenX){
+                groundX -= speed;
+            }
+        }
+        if(cmd.getLeft()){
+            rightDirection = false;
+            if(screenX != 0){
+                playerX -= speed;
+            }
+            if(screenX > minScreenX){
+                screenX -= speed;
+            }
+            if(screenX == minScreenX && groundX < 0){
+                groundX += speed;
+            }
+            minScreenX = groundX == 0 ? 0 : 180;
+        }
     }
 
     @Override
