ea31281627b579abbc2be56376c8473712b59b83
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-21 09:54:24 +0200

    added leftimage in the try

--- src/main/java/it/unibo/runwarrior/model/Guard.java
+++ src/main/java/it/unibo/runwarrior/model/Guard.java
@@ -1,6 +1,7 @@
 package it.unibo.runwarrior.model;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -8,12 +9,14 @@ import javax.imageio.ImageIO;
 import it.unibo.runwarrior.view.Handler;
 
 public class Guard extends EnemyImpl {
-
+    public BufferedImage rightImage, leftImage;
     public Guard(int x, int y, int width, int height, boolean solid, Handler handler) {
         super(x, y, width, height, solid, handler);
         setVelocityX(2);
         try{
-            image = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuardia.png"));
+            rightImage = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuardia.png"));
+            leftImage = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuard.png"));
+            image = rightImage;
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -26,8 +29,7 @@ public class Guard extends EnemyImpl {
 
     @Override
     public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        x +=velocityX
     }
 
 }
