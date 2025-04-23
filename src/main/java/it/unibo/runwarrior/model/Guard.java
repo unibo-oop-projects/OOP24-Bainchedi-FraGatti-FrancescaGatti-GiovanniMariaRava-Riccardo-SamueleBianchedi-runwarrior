b9bc399f9984ac2dab461c28b77dc6c2fb4424f5
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-23 12:18:51 +0200

    modified update and render method to support4image

--- src/main/java/it/unibo/runwarrior/model/Guard.java
+++ src/main/java/it/unibo/runwarrior/model/Guard.java
@@ -9,15 +9,21 @@ import javax.imageio.ImageIO;
 import it.unibo.runwarrior.view.Handler;
 
 public class Guard extends EnemyImpl {
-    public BufferedImage rightImage, leftImage;
+    public BufferedImage rightGuard, leftGuard, rightGuardMoving, leftGuardMoving;
     public int minX, maxX;
+
+    public int frameCounter = 0;
+    public boolean step = false;
+
     public Guard(int x, int y, int width, int height, boolean solid, Handler handler, int minX, int maxX) {
         super(x, y, width, height, solid, handler);
         setVelocityX(2);
         try{
-            rightImage = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuard.png"));
-            leftImage = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuard.png"));
-            image = rightImage;
+            rightGuard = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuard.png"));
+            leftGuard = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuard.png"));
+            rightGuardMoving = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuard.png"));
+            leftGuardMoving = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuard.png"));
+            image = rightGuard;
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -27,21 +33,31 @@ public class Guard extends EnemyImpl {
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(image, x, y, width, height, null);
+        BufferedImage currentImage;
+
+        if (velocityX > 0) {
+            currentImage = step ? rightGuardMoving : rightGuard;
+        } else {
+            currentImage = step ? leftGuardMoving : leftGuard;
+        }
+
+        g.drawImage(currentImage, x, y, width, height, null);
     }
 
     @Override
     public void update() {
         x += velocityX;
-        
-        if(x<=minX | x>= maxX -width){
-            velocityX = -velocityX;
 
-            if(velocityX>0){
-                image=rightImage;
-            }else{
-                image = leftImage;
-            }
+        // Cambia direzione se arriva ai limiti
+        if (x <= minX || x >= maxX - width) {
+            velocityX = -velocityX;
+        }
+
+        // Gestione "passo" animazione
+        frameCounter++;
+        if (frameCounter >= 20) { // cambia ogni 20 tick
+            step = !step;
+            frameCounter = 0;
         }
     }
 
