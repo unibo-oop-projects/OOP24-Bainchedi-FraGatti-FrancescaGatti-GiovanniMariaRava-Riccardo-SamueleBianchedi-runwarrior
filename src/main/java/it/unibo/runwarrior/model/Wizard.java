b2b33d1ec7da70798dac0d21803952d324b4d506
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-23 17:53:35 +0200

    tried to show the wizard

--- src/main/java/it/unibo/runwarrior/model/Wizard.java
+++ src/main/java/it/unibo/runwarrior/model/Wizard.java
@@ -3,16 +3,20 @@ package it.unibo.runwarrior.model;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
 import it.unibo.runwarrior.view.Handler;
 
 public class Wizard extends EnemyImpl {
-    public BufferedImage rightWizard, rightWizardMving, leftWizard, leftWizardMoving; 
+    public BufferedImage rightWizard, rightWizardMoving, leftWizard, leftWizardMoving; 
 
     public Wizard(int x, int y, int width, int height, boolean solid, Handler handler) {
         super(x, y, width, height, solid, handler);
         try {
-
-            
+            rightWizard= ImageIO.read(getClass().getResourceAsStream("/Wizard/rightWizard.png"));
+            rightWizardMoving= ImageIO.read(getClass().getResourceAsStream("/Wizard/rightWizardMoving.png"));
+            leftWizard= ImageIO.read(getClass().getResourceAsStream("/Wizard/leftWizard.png"));
+            leftWizardMoving= ImageIO.read(getClass().getResourceAsStream("/Wizard/leftWizardMoving.png")); 
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -20,8 +24,7 @@ public class Wizard extends EnemyImpl {
 
     @Override
     public void render(Graphics g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'render'");
+       g.drawImage(rightWizard, x, y,width,height, null);
     }
 
     @Override
