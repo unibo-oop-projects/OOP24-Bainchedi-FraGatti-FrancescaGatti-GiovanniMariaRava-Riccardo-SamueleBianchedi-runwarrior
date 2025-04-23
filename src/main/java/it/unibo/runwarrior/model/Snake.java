3aaae0b9da31cdf255e9707aff489908f32c8379
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-23 11:48:26 +0200

    write some space limits just for test purpose

--- src/main/java/it/unibo/runwarrior/model/Snake.java
+++ src/main/java/it/unibo/runwarrior/model/Snake.java
@@ -9,9 +9,11 @@ import it.unibo.runwarrior.view.Handler;
 
 public class Snake extends EnemyImpl{
     public BufferedImage rightSnake, rightSnakeMoving, leftSnake, leftSnakeMoving;
-
-    public Snake(int x, int y, int width, int height, boolean solid, Handler handler) {
+    public int minX, maxX;
+    public Snake(int x, int y, int width, int height, boolean solid, Handler handler, int minX, int maxX) {
         super(x, y, width, height, solid, handler);
+        this.minX = minX;
+        this.maxX = maxX;
         setVelocityX(3);
         try {
             rightSnake = ImageIO.read(getClass().getResourceAsStream("/Snake/rightSnake.png"));
