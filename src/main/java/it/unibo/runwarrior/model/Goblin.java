cf5689e1ea22f52fd0535be290c539076e01497b
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-04-29 12:24:40 +0200

    made render method

--- src/main/java/it/unibo/runwarrior/model/Goblin.java
+++ src/main/java/it/unibo/runwarrior/model/Goblin.java
@@ -10,8 +10,12 @@ import it.unibo.runwarrior.view.Handler;
 public class Goblin extends EnemyImpl{
     public BufferedImage rightGoblin, leftGoblin, rightGoblinMoving, leftGoblinMoving; 
     public int minX, maxX;
+
+    public int frameCounter = 0;
+    public boolean step = false;
     public Goblin(int x, int y, int width, int height, boolean solid, Handler handler, int minX, int maxX) {
         super(x, y, width, height, solid, handler);
+        setVelocityX(2);
         try {
             rightGoblin = ImageIO.read(getClass().getResourceAsStream("/Goblin/rightGoblin.png"));
             leftGoblin = ImageIO.read(getClass().getResourceAsStream("/Goblin/leftGoblin.png"));
@@ -24,8 +28,13 @@ public class Goblin extends EnemyImpl{
 
     @Override
     public void render(Graphics g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'render'");
+        BufferedImage currentImage;
+        if(velocityX>0){
+            currentImage = step ? rightGoblinMoving : rightGoblin;
+        }else{
+            currentImage = step ? leftGoblinMoving : leftGoblin;
+        }
+        g.drawImage(currentImage, x, y,width, height, null);
     }
 
     @Override
