515c99a9b87bc2541ca5b0ccd6a98305d9444abe
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2025-05-10 13:48:39 +0200

    menu buttons implemented

--- src/main/java/it/unibo/runwarrior/view/menu.java
+++ src/main/java/it/unibo/runwarrior/view/menu.java
@@ -1,13 +1,7 @@
 package it.unibo.runwarrior.view;
 
-
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*; 
+import java.awt.*;
 import java.awt.event.*;
 
 public class menu {
@@ -33,11 +27,35 @@ public class menu {
         pannelloSfondoMenu.setOpaque(false);
 
         ImageIcon immagineSfondo = new ImageIcon("C:\\Users\\Utente\\Desktop\\OOP24-runwarrior\\src\\main\\resources\\Menu\\sfondoMenu.png"); 
-        JLabel visualizzaSfondo = new JLabel(immagineSfondo); //il label può essere usato per mostrare semplici stringhe di testo o immagini (come icone) all'interno di un'interfaccia grafica
+        JLabel visualizzaSfondo = new JLabel(immagineSfondo); 
         visualizzaSfondo.setBounds(0,0,1280, 720); 
         pannelloSfondoMenu.add(visualizzaSfondo);  
 
         frameMenu.add(pannelloSfondoMenu);
+
+        final JPanel pannelloTastoPlay = new JPanel(); 
+        pannelloTastoPlay.setLayout(new FlowLayout(FlowLayout.CENTER));
+        pannelloTastoPlay.setOpaque(false);
+
+        final JButton playButton = new JButton("PLAY");
+        playButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){ 
+                pannelloTastoPlay.remove(playButton);
+                pannelloTastoPlay.add(new JButton("LIVELLO 1")); 
+                pannelloTastoPlay.add(new JButton("LIVELLO 2")); 
+                pannelloTastoPlay.add(new JButton("LIVELLO 3")); 
+
+                final JButton pulsanteNegozio = new JButton("NEGOZIO"); 
+                pulsanteNegozio.setBounds(1100, 20, 120, 40);  
+                pannelloSfondoMenu.add(pulsanteNegozio); 
+                pannelloTastoPlay.validate();
+            }
+            
+        });
+        pannelloTastoPlay.add(playButton);
+        pannelloTastoPlay.setBounds(430, 300, 400, 100); 
+        pannelloSfondoMenu.add(pannelloTastoPlay);
+        
         frameMenu.setVisible(true);
     }
 }
