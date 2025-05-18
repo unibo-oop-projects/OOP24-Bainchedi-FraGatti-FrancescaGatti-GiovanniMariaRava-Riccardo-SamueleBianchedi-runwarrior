2a8dfc191d6c60fd7d9decc83b42ffe3076eedaa
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2025-05-18 21:06:27 +0200

    class cronometro a little implemented

--- src/main/java/it/unibo/runwarrior/model/cronometro.java
+++ src/main/java/it/unibo/runwarrior/model/cronometro.java
@@ -1,5 +1,29 @@
 package it.unibo.runwarrior.model;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class cronometro {
-    
+    long tempoInizio; 
+    JLabel labelTempo;
+    Timer t; 
+
+    public cronometro(JLabel l){
+        labelTempo = l; 
+        t = new Timer(100, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               //azioni del timer 
+               long diffTempo = System.currentTimeMillis() - tempoInizio; 
+               final int decimiSec = (int) ((diffTempo % 1000) / 100); 
+               final int secondi = (int) ((diffTempo/1000) % 60); 
+               int minuti = (int) (diffTempo/60000 % 60); 
+               int ore = (int) (diffTempo / 3600000); 
+               String s = String.format("%d:%2d:%2d:%d", ore, minuti, secondi, decimiSec);
+               labelTempo.setText(s);
+               //ore : minuti : secondi : decimi secondo
+            }
+        });
+    }
 }
