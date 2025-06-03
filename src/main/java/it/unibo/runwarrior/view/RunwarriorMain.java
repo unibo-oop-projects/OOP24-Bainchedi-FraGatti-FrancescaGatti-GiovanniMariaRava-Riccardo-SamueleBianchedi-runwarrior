be725fb8bc1047d3b89ad531ea3d49f95e753ac7
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2025-06-03 21:05:24 +0200

    now when you run the main the first frame is the menu

--- src/main/java/it/unibo/runwarrior/view/RunwarriorMain.java
+++ src/main/java/it/unibo/runwarrior/view/RunwarriorMain.java
@@ -1,24 +1,11 @@
 package it.unibo.runwarrior.view;
 
-import javax.swing.JFrame;
+//import javax.swing.JFrame;
 
 public class RunwarriorMain{
 
-    private JFrame mainFrame;
-    private GameLoopPanel glp;
-
     public RunwarriorMain(){
-        mainFrame = new JFrame("runwarrior");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setLocationByPlatform(false);
-        mainFrame.setResizable(true);
-
-        glp = new GameLoopPanel();
-        glp.startGame();
-
-        mainFrame.add(glp);
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+        new Menu(); 
     }
     public static void main(String[] args){
         new RunwarriorMain();
--- src/main/java/it/unibo/runwarrior/view/menu.java
+++ src/main/java/it/unibo/runwarrior/view/menu.java
@@ -1,62 +1,87 @@
 package it.unibo.runwarrior.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*; 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class Menu {
-    public static void main(String[] args){
+public class Menu extends JPanel{
         //creo il frame e imposto il titolo 
-        final JFrame frameMenu = new JFrame(); 
-        frameMenu.setTitle("RUN WARRIOR");
-        frameMenu.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        frameMenu.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e){
-                int n = JOptionPane.showConfirmDialog(frameMenu, "do you really want to quit?", "Quitting...", JOptionPane.YES_NO_OPTION); 
-                if(n==JOptionPane.YES_OPTION){
-                    System.exit(0);
+        final JFrame frameMenu = new JFrame();
+        BufferedImage immagineSfondo = null;
+        public Menu(){
+            frameMenu.setTitle("RUN WARRIOR");
+            frameMenu.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+            frameMenu.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e){
+                    int n = JOptionPane.showConfirmDialog(frameMenu, "do you really want to quit?", "Quitting...", JOptionPane.YES_NO_OPTION); 
+                    if(n==JOptionPane.YES_OPTION){
+                        System.exit(0);
+                    }
                 }
+            });
+            frameMenu.setSize(1280, 720); 
+            frameMenu.setLocationRelativeTo(null); //per centrare la finestra
+ 
+            //ImageIcon immagineSfondo = new ImageIcon("C:\Users\Utente\Desktop\OOP24-runwarrior\src\main\resources\Menu\sfondoMenu.png"); 
+            try{
+                immagineSfondo = ImageIO.read(getClass().getResourceAsStream("/Menu/sfondoMenu.png"));
+            }catch(IOException e){
+                e.printStackTrace();
             }
-        });
-        frameMenu.setSize(1280, 720); 
-        frameMenu.setLocationRelativeTo(null); //per centrare la finestra
 
-        final JPanel pannelloSfondoMenu = new JPanel(); 
-        pannelloSfondoMenu.setLayout(null);
-        pannelloSfondoMenu.setOpaque(false);
+            final JPanel pannelloSfondoMenu = new JPanel(){
+                @Override
+                protected void paintComponent(Graphics g){
+                    super.paintComponent(g);
+                    if(immagineSfondo != null){
+                        g.drawImage(immagineSfondo, 0, 0, getWidth(), getHeight(), this); 
+                    }
+                }
+            };
+            pannelloSfondoMenu.setLayout(null);
+            pannelloSfondoMenu.setOpaque(true);
 
-        ImageIcon immagineSfondo = new ImageIcon("/Menu/sfondoMenu.png"); 
-        JLabel visualizzaSfondo = new JLabel(immagineSfondo); 
-        visualizzaSfondo.setBounds(0,0,1280, 720); 
-        pannelloSfondoMenu.add(visualizzaSfondo);  
+            final JPanel pannelloTastoPlay = new JPanel(); 
+            pannelloTastoPlay.setLayout(new FlowLayout(FlowLayout.CENTER));
+            pannelloTastoPlay.setOpaque(false);
+            pannelloTastoPlay.setBounds(430,300,400,100);
 
-        frameMenu.add(pannelloSfondoMenu);
+            final JButton playButton = new JButton("PLAY");
+            playButton.addActionListener(new ActionListener() {
+                private JFrame mainFrame;
+                private GameLoopPanel glp;
+                public void actionPerformed(ActionEvent e){ 
+                    pannelloTastoPlay.remove(playButton);
+                    mainFrame = new JFrame("runwarrior");
+                    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    mainFrame.setLocationByPlatform(false);
+                    mainFrame.setResizable(true);
 
-        final JPanel pannelloTastoPlay = new JPanel(); 
-        pannelloTastoPlay.setLayout(new FlowLayout(FlowLayout.CENTER));
-        pannelloTastoPlay.setOpaque(false);
+                    glp = new GameLoopPanel();
+                    glp.startGame();
+                    
+                    mainFrame.add(glp);
+                    mainFrame.pack();
+                    mainFrame.setVisible(true);
+                    //pannelloTastoPlay.add(new JButton("LIVELLO 1")); 
+                    //pannelloTastoPlay.add(new JButton("LIVELLO 2")); 
+                    //pannelloTastoPlay.add(new JButton("LIVELLO 3")); 
 
-        final JButton playButton = new JButton("PLAY");
-        playButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){ 
-                pannelloTastoPlay.remove(playButton);
-                pannelloTastoPlay.add(new JButton("LIVELLO 1")); 
-                pannelloTastoPlay.add(new JButton("LIVELLO 2")); 
-                pannelloTastoPlay.add(new JButton("LIVELLO 3")); 
-
-                final JButton pulsanteNegozio = new JButton("NEGOZIO"); 
-                pulsanteNegozio.setBounds(1100, 20, 120, 40);  
-                pannelloSfondoMenu.add(pulsanteNegozio); 
-                pannelloTastoPlay.validate();
-            }
+                    //final JButton pulsanteNegozio = new JButton("NEGOZIO"); 
+                    //pulsanteNegozio.setBounds(1100, 20, 120, 40);  
+                    //pannelloSfondoMenu.add(pulsanteNegozio); 
+                    pannelloTastoPlay.validate();
+                }
+            });
+            pannelloTastoPlay.add(playButton); 
+            pannelloSfondoMenu.add(pannelloTastoPlay);
             
-        });
-        pannelloTastoPlay.add(playButton);
-        pannelloTastoPlay.setBounds(430, 300, 400, 100); 
-        pannelloSfondoMenu.add(pannelloTastoPlay);
-        
-        frameMenu.setVisible(true);
-    }
+            frameMenu.setContentPane(pannelloSfondoMenu);
+            frameMenu.setVisible(true);
+        }
 }
 
