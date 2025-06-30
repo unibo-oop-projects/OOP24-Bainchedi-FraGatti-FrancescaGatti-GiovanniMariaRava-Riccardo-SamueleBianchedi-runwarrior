17b542b946a70b076299074fdb2c0e1d3e7652b0
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-06-30 17:44:46 +0200

    implement MapIMage method and modified constructor

--- src/main/java/it/unibo/runwarrior/controller/HandlerMapElement.java
+++ src/main/java/it/unibo/runwarrior/controller/HandlerMapElement.java
@@ -1,20 +1,56 @@
 package it.unibo.runwarrior.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import it.unibo.runwarrior.model.GameMap;
 import it.unibo.runwarrior.model.MapElement;
+import java.awt.image.BufferedImage;
 
 public class HandlerMapElement {
     
     public static final int HEIGHT = 480;
     public static final int TILE_SIZE = 48;
-    private MapElement[] blocks;
+    private List<MapElement> blocks;
     private int[][] map;
+    private Map<Integer, BufferedImage> mapBlock;
 
-    public HandlerMapElement(){
-        blocks = new MapElement[10];
+    public HandlerMapElement(GameMap gamemap){
+        blocks = new ArrayList<>();
         MapImage();
+        this.mapBlock = gamemap.getBlockImages();
+        this.map = gamemap.getMapData();
     }
 
+    /**
+     * 0 = sky
+     * 1 = grass
+     * 2 = terrain
+     * 3 = blocchi portale
+     * 4 = centro portale
+     * 5 = ostacolo
+     */
     public void MapImage() {
-        
+        for (var entry : mapBlock.entrySet()) {
+            MapElement newElement = new MapElement();
+            newElement.setImage(entry.getValue());
+            switch (entry.getKey()) {
+                case 0:
+                    newElement.setHarmless(true);
+                    newElement.setCollision(false);
+                    break;
+                case 1,2,3,4:
+                    newElement.setHarmless(true);
+                    newElement.setCollision(true);
+                    break;
+                case 5:
+                    newElement.setHarmless(false);
+                    newElement.setCollision(true);
+                    break;
+            }
+            blocks.add(newElement);
+
+        }
     } 
 }
