945fa9548942876f74eab8f7f3669138dd875779
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-06-30 15:43:28 +0200

    added th class that handles a map object

---
+++ src/main/java/it/unibo/runwarrior/model/MapElement.java
@@ -0,0 +1,33 @@
+package it.unibo.runwarrior.model;
+
+import java.awt.image.BufferedImage;
+
+public class MapElement {
+    private BufferedImage image;
+    private boolean collision;
+    private boolean harmless;
+
+    public void setImage(BufferedImage im){
+        this.image = im;
+    }
+
+    public void setCollision(boolean col){
+        this.collision = col;
+    }
+
+    public void setHarmless(boolean harm){
+        this.harmless = harm;
+    }
+
+    public BufferedImage getImage(){
+        return image;
+    }
+
+    public boolean getCollision(){
+        return collision;
+    }
+
+    public boolean getHarmless(){
+        return harmless;
+    }
+}
