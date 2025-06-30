1e3ea5efce1c4d9b52c5e4e6b2636d39c121d329
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2025-06-30 18:28:30 +0200

    CoinController implemented and coin positions of map 1 in txt file added

--- src/main/java/it/unibo/runwarrior/model/Coin.java
+++ src/main/java/it/unibo/runwarrior/model/Coin.java
@@ -1,15 +1,30 @@
 package it.unibo.runwarrior.model;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Coin {
     private int row; 
     private int col; 
     private boolean collected; 
+    BufferedImage coinImage; 
 
     public Coin(int row, int col){
         this.row = row; 
         this.col = col; 
         this.collected = false; 
     }
+    
+    public void loadCoinImage(){
+        try{
+                coinImage = ImageIO.read(getClass().getResourceAsStream("/Coins/CoinSmall.png"));
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+    }
+
     public int getRow(){
         return row; 
     }
