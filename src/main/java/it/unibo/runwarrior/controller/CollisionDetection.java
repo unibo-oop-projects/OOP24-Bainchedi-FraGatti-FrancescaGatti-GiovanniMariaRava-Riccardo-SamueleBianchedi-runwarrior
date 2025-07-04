4c09609c6280714a7bb975d838f7818abfe47803
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-07-04 19:47:23 +0200

    created a sketchy class to handle the collisions of the player in the map

---
+++ src/main/java/it/unibo/runwarrior/controller/CollisionDetection.java
@@ -0,0 +1,53 @@
+package it.unibo.runwarrior.controller;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+//da creare il vettore bidimensionale e considerare i blocchi come degli oggetti (utile per capire se è solido o ostacoloCavaVita [booleani])
+import it.unibo.runwarrior.model.MapElement;
+
+public class CollisionDetection {
+    
+    private int map[][];
+    private List<MapElement> blocks = new ArrayList<>();
+    private ArrayList<String> directions = new ArrayList<>();
+    private int playerSpeed;
+
+    public CollisionDetection(int map[][]){
+        this.map = map;
+    }
+
+    public boolean touchSolid(int x, int y){
+        int[][] blockIndexes = map;
+        float indexXtile = x / 48;
+        float indexYtile = y / 48;
+        int blockIndex = blockIndexes[(int) indexYtile][(int) indexXtile];
+        if(blocks.get(blockIndex).getCollision()){
+            this.directions.add(checkCollisionDirection(x, y, indexXtile, indexYtile));
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String checkCollisionDirection(int x, int y, float indexXtile, float indexYtile){
+        String direction = "";
+        int tileX = ((int) indexXtile) * 48;
+        int tileY = ((int) indexYtile) * 48;
+        Rectangle tileRec = new Rectangle(tileX, tileY, 48, 48);
+        if(y == tileRec.y && (x >= tileRec.x && x <= tileRec.x + tileRec.width)){
+            direction = "up";
+        }
+        else if((y == tileRec.y + tileRec.height - 1) && (x >= tileRec.x && x <= tileRec.x + tileRec.width)){
+            direction = "down";
+        }
+        else if(x - playerSpeed < tileRec.x){
+            direction = "right";
+        }
+        else if(x + playerSpeed > tileRec.x + tileRec.width){
+            direction = "left";
+        }
+        return direction;
+    }
+}
