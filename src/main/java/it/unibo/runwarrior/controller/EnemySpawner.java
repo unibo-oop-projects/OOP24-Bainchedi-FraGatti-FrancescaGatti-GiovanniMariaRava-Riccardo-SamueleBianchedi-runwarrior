9e481055af5ad3a66335b686dfb2e270ae0bff67
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-10 18:09:06 +0200

    created first method to load file

--- src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
+++ src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
@@ -1,18 +1,49 @@
 package it.unibo.runwarrior.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
-import java.util.logging.Handler;
 
 import it.unibo.runwarrior.model.EnemyImpl;
 import it.unibo.runwarrior.view.GameLoopPanel;
+import it.unibo.runwarrior.controller.EnemyHandler;
 
 public class EnemySpawner {
-    private Handler handler;
+    private EnemyHandler handler;
     private GameLoopPanel glp;
     private List<EnemyImpl> enemies;
-    public EnemySpawner(Handler handler, GameLoopPanel glp) {
+    public EnemySpawner(EnemyHandler handler, GameLoopPanel glp) {
         this.handler = handler;
         this.glp = glp;
     }
-    
+      /*
+     * sta classe dovrebbe leggere un file che ha le coordinate di tutti i nemici e che contiene tre numeri perriga:
+     * tipo di nemico, coordinata x, coordinata y.
+     */
+    public void loadEnemiesFromStream(InputStream is) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.trim().split(",");
+                if (parts.length != 3) continue;
+
+                int type = Integer.parseInt(parts[0]);
+                int x = Integer.parseInt(parts[1]);
+                int y = Integer.parseInt(parts[2]);
+
+                EnemyImpl enemy = createEnemyByType(type, x, y);
+                if (enemy != null) {
+                    handler.addEnemy(enemy);
+                    
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.err.println("Errore durante il caricamento dei nemici: " + e.getMessage());
+        }
+        
+        
+    }
+
 }
