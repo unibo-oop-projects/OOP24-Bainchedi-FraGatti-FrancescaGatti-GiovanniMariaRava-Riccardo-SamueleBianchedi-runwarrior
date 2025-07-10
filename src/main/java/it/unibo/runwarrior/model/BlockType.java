fe25d3aad31e794114284e4edf9fb03cc9d60329
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-10 17:19:10 +0200

    fixed some mapelements problem

--- src/main/java/it/unibo/runwarrior/model/BlockType.java
+++ src/main/java/it/unibo/runwarrior/model/BlockType.java
@@ -34,4 +34,39 @@ public class BlockType {
     public boolean getHarmless(){
         return harmless;
     }
+
+/* 
+    private int id; // Identificatore del blocco
+    private String name;
+    private boolean solid; // Indica se il blocco è solido (non attraversabile)+
+    private boolean harmfull; // Indica se il blocco è dannoso (causa danni al giocatore)
+    private BufferedImage image; // Immagine associata al blocco
+
+    public MapElement(int id, String name, boolean solid, boolean harmfull, BufferedImage image) {
+        this.id = id;
+        this.solid = solid;
+        this.harmfull = harmfull;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isSolid() {
+        return solid;
+    }
+
+    public boolean isHarmfull() {
+        return harmfull;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+    */
 }
