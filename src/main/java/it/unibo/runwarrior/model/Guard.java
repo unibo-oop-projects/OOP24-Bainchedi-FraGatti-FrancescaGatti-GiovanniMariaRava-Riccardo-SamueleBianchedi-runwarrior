19c042e7c628b646bd4a35c72d954787640952ec
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-10 17:46:11 +0200

    added new meythod for enemy collision with obstacl

--- src/main/java/it/unibo/runwarrior/model/Guard.java
+++ src/main/java/it/unibo/runwarrior/model/Guard.java
@@ -52,7 +52,7 @@ public class Guard extends EnemyImpl {
     public void update() {
         x += velocityX;
 
-        //checkMapCollision(glp.getMapHandler().getCollisionRectagles());
+        checkMapCollision(glp.getMapHandler().getCollisionRectagles());
         
         frameCounter++;
         if (frameCounter >= 20) { 
