1f228c702c52467b53b9070ec1b9f6640f305142
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-07-10 20:11:14 +0200

    created a class that handle only the player movement

---
+++ src/main/java/it/unibo/runwarrior/controller/CharacterMovementHandler.java
@@ -0,0 +1,167 @@
+package it.unibo.runwarrior.controller;
+
+import it.unibo.runwarrior.view.GameLoopPanel;
+import it.unibo.runwarrior.model.Character;
+
+public class CharacterMovementHandler {
+    private GameLoopPanel glp;
+    private CharacterComand cmd;
+    private CollisionDetection collisionDetection;
+    private Character player;
+    private HandlerMapElement mapHandler;
+
+    public static final int START_X = 96;
+    private int startY;
+    private int maxJump;
+    private int midJump;
+    protected int sizeCharacter;
+    private int toTouchFloor = 2;
+    
+    private final int minScreenX = 0;//y IN CUI SI FERMA IL PLAYER NELLO SCHERMO
+    private int maxScreenX;//x IN CUI SI FERMA IL PLAYER NELLO SCHERMO
+    protected int playerX = START_X;//POSIZIONE ORIZZONTALE DEL PLAYER NELLA MAPPA
+    protected int playerY;// * VERTICALE
+    private int screenX = START_X;//POSIZIONE ORIZZONTALE DEL PLAYER NELLO SCHERMO
+    private int screenY;// * VERITCALE (NON USATA PERCHè LA POSIZIONE VERTICALE è DATA SOLO DAL SALTO)
+    private boolean hitHead;
+    private boolean jumpKill;
+    private boolean descend;
+    private boolean handleDoubleCollision;
+
+    private boolean rightDirection = true;
+    private int speed = 5;
+    private int speedJumpUP = 12; 
+    private int speedJumpDown = 6;
+    private int groundX = 0;//variabile che permette lo scorrimento della mappa
+
+    public CharacterMovementHandler(GameLoopPanel panel, Character player, CharacterComand cmd, CollisionDetection collDet, HandlerMapElement hM){
+        this.glp = panel;
+        this.cmd = cmd;
+        this.collisionDetection = collDet; 
+        this.player = player;
+        this.mapHandler = hM;
+        setStartY(mapHandler.getFirstY(), mapHandler.getTileSize());
+    }
+
+    private void setStartY(int y, int tileSize){
+        startY = y + toTouchFloor;
+        playerY = startY;
+        screenY = startY;
+        sizeCharacter = tileSize*2;
+        maxJump = startY - (sizeCharacter*5/2);
+        midJump = startY - (sizeCharacter*3/2);
+    }
+
+    public void movePlayer(){
+        maxScreenX = glp.getWidth() / 2;
+        String collisionDir = "";
+        collisionDir = collisionDetection.checkCollision(player);
+        System.out.println(collisionDir);
+
+        hitHead = collisionDir.equals("down") ? true : false;
+        if(hitHead){
+            cmd.setJump(false);
+        }
+        jump(cmd.isJumping(), maxJump);
+        handleDoubleCollision = (collisionDir.equals("up") || collisionDir.equals("down")) && descend ? true : false;
+        if(jumpKill){
+            jumpAfterKill();
+        }
+
+        if(cmd.getRight() && !cmd.getLeft()){
+            rightDirection = true;
+            if(!collisionDir.equals("right") && !handleDoubleCollision){
+                playerX += speed;
+                if(screenX < maxScreenX){
+                    screenX += speed;
+                }
+                else{
+                    groundX -= speed;
+                    mapHandler.setShift(groundX);
+                }
+            }
+        }
+        if(cmd.getLeft() && !cmd.getRight()){
+            rightDirection = false;
+            if(!collisionDir.equals("left") && !handleDoubleCollision){
+                if(screenX > 0){
+                    playerX -= speed;
+                }
+                if(screenX > minScreenX){
+                    screenX -= speed;
+                }
+            }
+        }
+        player.updatePlayerPosition();
+    }
+
+    public void jump(boolean isJump, int jumpHeight){
+        if(isJump && !descend){
+            if(playerY > jumpHeight){
+                playerY -= speedJumpUP;
+            }
+            else{
+                playerY = jumpHeight;
+                cmd.setJump(false);
+            }
+        }
+        else{
+            if(collisionDetection.isInAir(player) && !jumpKill){
+                descend = true;
+                playerY += speedJumpDown;
+                System.out.println("scendo");
+            }
+            else{
+                descend = false;
+                cmd.setDoubleJump(false);
+            }
+            updateJumpVariable();
+        }
+    }
+
+    public void setJumpKill(){
+        this.jumpKill = true;
+    }
+
+    public void jumpAfterKill(){
+        if(playerY > midJump){
+            playerY -= speedJumpUP;
+        }
+        else{
+            playerY = midJump;
+            jumpKill = false;
+            cmd.setJump(false);
+        }
+    }
+
+    public void updateJumpVariable(){
+        maxJump = (startY - (sizeCharacter*5/2)) + (playerY - startY);
+        midJump = (startY - (sizeCharacter*3/2)) + (playerY - startY);
+    }
+
+    public void setLocationAfterPowerup(int x, int y, int realx) {
+        this.playerY = y;
+        this.playerX = realx;
+        this.screenX = x;
+    }
+
+    public boolean getRightDirection(){
+        return this.rightDirection;
+    }
+
+    public int getPlX() {
+        return this.playerX;
+    }
+
+    public int getPlY() {
+        return this.playerY;
+    }
+
+    public int getScX() {
+        return this.screenX;
+    }
+
+    public int getScY() {
+        return this.screenY;
+    }
+}
