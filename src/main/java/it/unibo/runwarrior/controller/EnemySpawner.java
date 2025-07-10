16f36ebb470f7c7872fb983f8a03d023b206dfab
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-10 18:04:56 +0200

    wrote enemyspwaner constructor

--- src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
+++ src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
@@ -1,5 +1,18 @@
 package it.unibo.runwarrior.controller;
 
-public class EnemySpawner {
+import java.util.List;
+import java.util.logging.Handler;
 
+import it.unibo.runwarrior.model.EnemyImpl;
+import it.unibo.runwarrior.view.GameLoopPanel;
+
+public class EnemySpawner {
+    private Handler handler;
+    private GameLoopPanel glp;
+    private List<EnemyImpl> enemies;
+    public EnemySpawner(Handler handler, GameLoopPanel glp) {
+        this.handler = handler;
+        this.glp = glp;
+    }
+    
 }
