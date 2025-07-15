00519bae2a728cb9774c74982cbc7f196c456e42
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-15 10:37:41 +0200

    implemented load method

--- src/main/java/it/unibo/runwarrior/model/GameSaveManager.java
+++ src/main/java/it/unibo/runwarrior/model/GameSaveManager.java
@@ -1,6 +1,9 @@
 package it.unibo.runwarrior.model;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -29,6 +32,29 @@ public class GameSaveManager {
             writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static GameSaveManager loadFromFile(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return null;
+        }
+
+        GameSaveManager gsm = new GameSaveManager();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            
+            if ((line = reader.readLine()) != null) {
+                gsm.livelliCompletati = Integer.parseInt(line.trim());
+                gsm.moneteRaccolte = Integer.parseInt(line.trim());
+                gsm.skinPremiumSbloccata = Boolean.parseBoolean(line.trim());
+            }
+
+            return gsm;
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
