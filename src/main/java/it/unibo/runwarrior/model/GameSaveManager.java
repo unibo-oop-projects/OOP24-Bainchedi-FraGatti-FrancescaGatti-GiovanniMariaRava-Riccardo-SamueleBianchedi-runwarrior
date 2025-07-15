b8f9c97563acd75de659fda6a4c95e651384b95d
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-15 10:33:17 +0200

    added method to save

--- src/main/java/it/unibo/runwarrior/model/GameSaveManager.java
+++ src/main/java/it/unibo/runwarrior/model/GameSaveManager.java
@@ -1,5 +1,9 @@
 package it.unibo.runwarrior.model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GameSaveManager {
     private static final String SAVE_FILE = "game_save.txt";
 
@@ -8,4 +12,23 @@ public class GameSaveManager {
     private boolean skinPremiumSbloccata;
 
     private static GameSaveManager instance;
+
+    private GameSaveManager() {
+        livelliCompletati = 0;
+        moneteRaccolte = 0;
+        skinPremiumSbloccata = false;
+    }
+
+    private void saveGame() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(SAVE_FILE))) {
+            writer.write(Integer.toString(livelliCompletati));
+            writer.newLine();
+            writer.write(Integer.toString(moneteRaccolte));
+            writer.newLine();
+            writer.write(Boolean.toString(skinPremiumSbloccata));
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
