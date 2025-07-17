069879c6ec2a37b7539993c70d9ad18a594920f2
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-17 17:28:13 +0200

    added constant to render enemy in the floor

--- src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
+++ src/main/java/it/unibo/runwarrior/controller/EnemySpawner.java
@@ -21,6 +21,7 @@ import it.unibo.runwarrior.view.GameLoopPanel;
 import it.unibo.runwarrior.controller.EnemyHandler;
 
 public class EnemySpawner {
+    private final static int TO_TOUCH_FLOOR = 8;
     private EnemyHandler handler;
     private GameLoopPanel glp;
     private List<EnemyImpl> enemies;
@@ -51,16 +52,7 @@ public class EnemySpawner {
                 int tiley = Integer.parseInt(parts[2]);
                 int x=tilex*glp.getMapHandler().getTileSize();
                 int y = tiley *glp.getMapHandler().getTileSize();
-                spawnPoints.add(new EnemySpawnPoints(type,tilex,tiley));
-                /*
-                 * Questa parte andrebbe tolta ma devo capire con riky come gestire i nemici quindi rimane qua per ora
-                 * EnemyImpl enemy = createEnemyByType(type, x, y);
-                 * if (enemy != null) {
-                 *     handler.addEnemy(enemy);
-                    System.out.println("Caricato nemico: "+i);
-                    i++;
-                }
-                 */ 
+                spawnPoints.add(new EnemySpawnPoints(type,tilex,tiley)); 
             }
         } catch (IOException | NumberFormatException e) {
             System.err.println("Errore durante il caricamento dei nemici: " + e.getMessage());
@@ -81,7 +73,7 @@ public class EnemySpawner {
             int enemyX = spawnPoint.x()*tileSize;
             if (enemyX >= screenLeft && enemyX <= screenRight && !spawnedEnemies.contains(spawnPoint)) {
                 System.out.println("Spawning enemy of type " + spawnPoint.type() + " at (" + enemyX + ", " + spawnPoint.y()*tileSize + ")");
-                EnemyImpl enemy = createEnemyByType(spawnPoint.type(), enemyX, spawnPoint.y()*tileSize);
+                EnemyImpl enemy = createEnemyByType(spawnPoint.type(), enemyX, (spawnPoint.y()*tileSize) + TO_TOUCH_FLOOR);
                 System.out.println("Enemy created: " + enemy);
                 if (enemy != null) {
                     handler.addEnemy(enemy);
