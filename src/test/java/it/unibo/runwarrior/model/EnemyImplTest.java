2f45569b747a0a8b275cc97269f0f91b48050e05
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-19 15:55:20 +0200

    created EnemyImplTest  and implemented beforeach

---
+++ src/test/java/it/unibo/runwarrior/model/EnemyImplTest.java
@@ -0,0 +1,23 @@
+package it.unibo.runwarrior.model.enemy;
+
+public class EnemyImplTest {
+    private EnemyImpl enemy;
+    private GameLoopPanel glp;
+    private EnemyHandler enemyHandler;
+
+    /**
+     * Sets up a new EnemyImpl instance.
+     */
+    @BeforeEach
+    void setUp() {
+        glp = new GameLoopPanel();
+        enemyHandler = new EnemyHandler(mockPanel);
+        enemy = new EnemyImpl(50, 100, 64, 64, true, enemyHandler, glp) {
+            @Override
+            public void update() {
+                //per i primi test non so ancora se voglio cambiargli la posizione, nei primi sicurocontrollo valori fissi
+            }
+        };
+    }
+
+}
