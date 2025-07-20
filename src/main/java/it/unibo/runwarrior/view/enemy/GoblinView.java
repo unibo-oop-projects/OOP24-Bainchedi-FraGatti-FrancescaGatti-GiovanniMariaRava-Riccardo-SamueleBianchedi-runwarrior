2dff84088078916aad39875c08255a30188452c8
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-20 15:01:00 +0200

    created GoblinView class

---
+++ src/main/java/it/unibo/runwarrior/view/enemy/GoblinView.java
@@ -0,0 +1,22 @@
+package it.unibo.runwarrior.view.enemy;
+
+import java.awt.Graphics;
+import java.io.IOException;
+
+import it.unibo.runwarrior.model.enemy.EnemyImpl;
+
+public class GoblinView implements EnemyView{
+
+    @Override
+    public void loadResources() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadResources'");
+    }
+
+    @Override
+    public void render(Graphics g, EnemyImpl enemy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+    
+}
