3c2c938ce3cc5ce801fc7b8a5dc44a5922d8f471
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-20 14:50:50 +0200

    implemented interface Enemy Impl

--- src/main/java/it/unibo/runwarrior/view/enemy/EnemyView.java
+++ src/main/java/it/unibo/runwarrior/view/enemy/EnemyView.java
@@ -1,5 +1,20 @@
 package it.unibo.runwarrior.view.enemy;
 
-public class EnemyView {
-    
-}
+import java.io.IOException;
+import java.awt.Graphics;
+import it.unibo.runwarrior.model.enemy.EnemyImpl;
+
+public interface EnemyView {
+
+    /**
+     * @throws IOException
+     * Load all the images of the enemy, throws IOException when the image is not found
+     */
+    public void loadResources() throws IOException;
+    /**
+     * @param g
+     * @param enemy
+     * Render the image of the enemy
+     */
+    public void render(Graphics g, EnemyImpl enemy);
+} 
