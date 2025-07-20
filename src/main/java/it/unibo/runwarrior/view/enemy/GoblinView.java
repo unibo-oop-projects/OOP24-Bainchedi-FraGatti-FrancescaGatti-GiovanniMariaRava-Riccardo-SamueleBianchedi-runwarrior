aa4f70cc5518d2a43d643645d76bcf59c84c05e5
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-20 15:02:19 +0200

    implemented interface method

--- src/main/java/it/unibo/runwarrior/view/enemy/GoblinView.java
+++ src/main/java/it/unibo/runwarrior/view/enemy/GoblinView.java
@@ -1,22 +1,52 @@
 package it.unibo.runwarrior.view.enemy;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
 import it.unibo.runwarrior.model.enemy.EnemyImpl;
+import it.unibo.runwarrior.view.GameLoopPanel;
 
 public class GoblinView implements EnemyView{
-
+    private BufferedImage rightGoblin;
+    private BufferedImage leftGoblin;
+    private BufferedImage rightGoblinMoving;
+    private BufferedImage leftGoblinMoving;
+    private BufferedImage image;
+    private GameLoopPanel glp;
+    public GoblinView( GameLoopPanel glp) {
+        this.glp = glp;
+        try {
+            loadResources();
+        } catch (IOException e){
+            e.printStackTrace();
+        } 
+    }
     @Override
     public void loadResources() throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadResources'");
+        rightGoblin = ImageIO.read(getClass().getResourceAsStream("/Goblin/rightGoblin.png"));
+        leftGoblin = ImageIO.read(getClass().getResourceAsStream("/Goblin/leftGoblin.png"));
+        rightGoblinMoving = ImageIO.read(getClass().getResourceAsStream("/Goblin/rightGoblinMoving.png"));
+        leftGoblinMoving = ImageIO.read(getClass().getResourceAsStream("/Goblin/leftGoblinMoving.png"));
+        image = rightGoblin;
     }
 
     @Override
     public void render(Graphics g, EnemyImpl enemy) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'render'");
+        BufferedImage currentImage;
+        if(enemy.velocityX == 0){
+            currentImage = image;
+        }else if(enemy.velocityX>0){
+            currentImage = enemy.step ? rightGoblinMoving : rightGoblin;
+        }else{
+            currentImage = enemy.step ? leftGoblinMoving : leftGoblin;
+        }
+        
+        int shift = glp.getMapHandler().getShift();  
+        g.drawImage(currentImage, enemy.x+shift, enemy.y, enemy.width, enemy.height, null);
+        
     }
     
 }
