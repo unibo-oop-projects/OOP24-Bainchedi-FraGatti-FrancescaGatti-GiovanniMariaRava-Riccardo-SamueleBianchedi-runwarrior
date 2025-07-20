662bbf24390de7dc463480e545f98958d6e9062c
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-20 14:54:10 +0200

    implemented interface method

--- src/main/java/it/unibo/runwarrior/view/enemy/GuardView.java
+++ src/main/java/it/unibo/runwarrior/view/enemy/GuardView.java
@@ -1,12 +1,17 @@
 package it.unibo.runwarrior.view.enemy;
 
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import it.unibo.runwarrior.model.enemy.EnemyImpl;
 import it.unibo.runwarrior.view.GameLoopPanel;
 
 
-public class GuardView {
+public class GuardView implements EnemyView {
     private BufferedImage rightGuard;
     private BufferedImage leftGuard;
     private BufferedImage rightGuardMoving;
@@ -22,5 +27,33 @@ public class GuardView {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void loadResources() throws IOException {
+        rightGuard = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuard.png"));
+        leftGuard = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuard.png"));
+        rightGuardMoving = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightGuardMoving.png"));
+        leftGuardMoving = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftGuardMoving.png"));
+        rightGuardRunning = ImageIO.read(getClass().getResourceAsStream("/Guardia/rightRunningGuard.png"));
+        leftGuardRunning = ImageIO.read(getClass().getResourceAsStream("/Guardia/leftRunningGuard.png"));
+        image = rightGuard;
+    }
+
+    @Override
+    public final void render(final Graphics g, EnemyImpl enemy) {
+        BufferedImage currentImage;
+        if (enemy.velocityX == 0) {
+            currentImage = image;
+        } else if (enemy.velocityX > 0) {
+            currentImage = enemy.step ? rightGuardMoving : rightGuardRunning;
+            image = rightGuard;
+        } else {
+            currentImage = enemy.step ? leftGuardMoving : leftGuardRunning;
+            image = leftGuard;
+        }
+        int shift = glp.getMapHandler().getShift();
+        //System.out.println("SNAKE XS: " + (x+shift)+ "X:"+ x);
+        g.drawImage(currentImage, enemy.x + shift, enemy.y, enemy.width, enemy.height, null);
     }
 }
