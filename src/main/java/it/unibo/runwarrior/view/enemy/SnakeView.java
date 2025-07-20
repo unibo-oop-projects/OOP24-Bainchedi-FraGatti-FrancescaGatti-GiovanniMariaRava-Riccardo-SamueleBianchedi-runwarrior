df8e077368acce0919a1449f48b1d9a04553c3e6
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-20 14:56:26 +0200

    made constructor

--- src/main/java/it/unibo/runwarrior/view/enemy/SnakeView.java
+++ src/main/java/it/unibo/runwarrior/view/enemy/SnakeView.java
@@ -1,11 +1,27 @@
 package it.unibo.runwarrior.view.enemy;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import it.unibo.runwarrior.model.enemy.EnemyImpl;
+import it.unibo.runwarrior.view.GameLoopPanel;
 
 public class SnakeView implements EnemyView{
+    private BufferedImage rightSnake;
+    private BufferedImage rightSnakeMoving;
+    private BufferedImage leftSnake;
+    private BufferedImage leftSnakeMoving;
+    private BufferedImage poisonImage;
+    private GameLoopPanel glp;
+    public SnakeView(GameLoopPanel glp) {
+        this.glp = glp;
+        try {
+            loadResources();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void loadResources() throws IOException {
