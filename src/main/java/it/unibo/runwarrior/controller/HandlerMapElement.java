69b5e5c99b15b015fca931aa77e55f0fb19677c4
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-22 19:23:27 +0200

    fixed map and images loading

--- src/main/java/it/unibo/runwarrior/controller/HandlerMapElement.java
+++ src/main/java/it/unibo/runwarrior/controller/HandlerMapElement.java
@@ -53,7 +53,7 @@ public class HandlerMapElement {
                     newElement.setHarmless(true);
                     newElement.setCollision(true);
                     break;
-                case 5:
+                case 5,6:
                     newElement.setHarmless(false);
                     newElement.setCollision(true);
                     break;
