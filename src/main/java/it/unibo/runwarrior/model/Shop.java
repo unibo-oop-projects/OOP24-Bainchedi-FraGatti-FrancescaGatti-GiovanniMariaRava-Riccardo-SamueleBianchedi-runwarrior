05bc4dad09ae1fcb1fd12eecc32645b0f29e99f5
Francesca Gatti <francesca.gatti15@studio.unibo.it>
2025-07-25 12:37:53 +0200

    shop view implemented

--- src/main/java/it/unibo/runwarrior/model/Shop.java
+++ src/main/java/it/unibo/runwarrior/model/Shop.java
@@ -1,11 +1,10 @@
 package it.unibo.runwarrior.model;
 
-import java.util.List;
 
 public class Shop {
     private final Skin newPremiumSkin;
 
-    public Shop(List<Skin> skins){
+    public Shop(){
         this.newPremiumSkin = new Skin("WIZARD", 50, GameSaveManager.getInstance().isSkinPremiumSbloccata()); 
     }
 
