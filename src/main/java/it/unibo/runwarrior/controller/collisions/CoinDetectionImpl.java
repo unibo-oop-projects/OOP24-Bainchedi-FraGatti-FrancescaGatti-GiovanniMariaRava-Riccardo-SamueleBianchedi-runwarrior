86c9b653384da7aeca6f76c093f46010354c8146
Riccardo <riccardo.cornacchia@studio.unibo.it>
2025-07-26 19:34:18 +0200

    created Coin collision detection class

---
+++ src/main/java/it/unibo/runwarrior/controller/collisions/CoinDetectionImpl.java
@@ -0,0 +1,42 @@
+package it.unibo.runwarrior.controller.collisions;
+
+import java.awt.Rectangle;
+import java.util.Iterator;
+import java.util.List;
+
+import it.unibo.runwarrior.controller.CoinController;
+import it.unibo.runwarrior.controller.ScoreController;
+import it.unibo.runwarrior.model.Coin;
+import it.unibo.runwarrior.model.player.Character;
+
+public class CoinDetectionImpl {
+
+    private int tileSize;
+    private CoinController coinC;
+    private ScoreController score;
+
+    public CoinDetectionImpl(int tileSize, CoinController coinC, ScoreController score){
+        this.tileSize = tileSize;
+        this.coinC = coinC;
+        this.score = score;
+    }
+    
+    public void controlCoinCollision(Character player){
+        Rectangle playerRectangle = player.getArea();
+        Iterator<Coin> it = coinC.getCoinList().iterator();
+        while(it.hasNext()){
+            Coin coin = it.next();
+            if (!coin.isCollected()){
+                Rectangle coinRectangle = coin.getRectangle(tileSize);
+                if (playerRectangle.intersects(coinRectangle)){
+                    coin.collect();
+                    coinC.increaseCoinsCollected();
+                    if(score != null){
+                        score.addCoin();
+                    }
+                    it.remove();
+                }
+            }
+        }
+    }
+}
