cc741592c1deb011723df1878eb105c00ea5dd78
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-27 23:02:18 +0200

    checkstyle

--- src/main/java/it/unibo/runwarrior/view/enemy/api/EnemyView.java
+++ src/main/java/it/unibo/runwarrior/view/enemy/api/EnemyView.java
@@ -13,14 +13,14 @@ import java.awt.Graphics;
 public interface EnemyView {
     /**
      * Load all the images of the enemy.
-     * 
+     *
      * @throws IOException when the image is not found
-     */   
+     */
     void loadResources() throws IOException;
 
     /**
      * Render the image of the enemy.
-     * 
+     *
      * @param g is the graphics that method use to render
      * @param enemy is the one rendered
      */
