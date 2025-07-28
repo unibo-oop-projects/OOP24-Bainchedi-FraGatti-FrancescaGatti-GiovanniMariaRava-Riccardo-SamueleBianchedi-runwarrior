c5b102316578201f9195756cae92477fced3bd9c
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-28 18:10:25 +0200

    added comments

--- src/test/java/it/unibo/runwarrior/view/enemy/impl/TestGoblinView.java
+++ src/test/java/it/unibo/runwarrior/view/enemy/impl/TestGoblinView.java
@@ -9,24 +9,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.runwarrior.controller.GameLoopController;
-
+/**
+ * Test if the images are loaded correctly and view is ok.
+ */
 public class TestGoblinView {
+    private static final int DIM_IMAGES = 36;
     private GoblinView goblinView;
     private GameLoopController glc;
     private JFrame mainFrame;
+    /**
+     * Create a new GameLoopController for the view.
+     */
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         glc = new GameLoopController(mainFrame, "tryMap.txt", "Map2/forest_theme.txt", 
                             "/Map2/enemiesMap2.txt", "/Coins/CoinCoordinates_map2.txt");
         goblinView = new GoblinView(glc);
     }
 
+    /**
+     * Test the right uploading of the images.
+     */
     @Test
-    public void testLoadResources(){
+    public void testLoadResources() {
         assertNotNull(goblinView.getRightGoblin());
         assertNotNull(goblinView.getLeftGoblin());
         assertNotNull(goblinView.getRightGoblinMoving());
         assertNotNull(goblinView.getLeftGoblinMoving());
-        assertEquals(36, goblinView.getRightGoblin().getWidth());;
+        assertEquals(DIM_IMAGES, goblinView.getRightGoblin().getWidth());;
     }
 }
