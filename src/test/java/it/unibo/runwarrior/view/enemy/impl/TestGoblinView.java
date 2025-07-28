de874618e68855a00409a99b41182231c2c1f475
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-07-28 11:16:13 +0200

    modified glc constructor

--- src/test/java/it/unibo/runwarrior/view/enemy/impl/TestGoblinView.java
+++ src/test/java/it/unibo/runwarrior/view/enemy/impl/TestGoblinView.java
@@ -3,6 +3,8 @@ package it.unibo.runwarrior.view.enemy.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import javax.swing.JFrame;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +13,10 @@ import it.unibo.runwarrior.controller.GameLoopController;
 public class TestGoblinView {
     private GoblinView goblinView;
     private GameLoopController glc;
+    private JFrame mainFrame;
     @BeforeEach
     public void setUp(){
-        glc = new GameLoopController("tryMap.txt", "Map2/forest_theme.txt", 
+        glc = new GameLoopController(mainFrame, "tryMap.txt", "Map2/forest_theme.txt", 
                             "/Map2/enemiesMap2.txt", "/Coins/CoinCoordinates_map2.txt");
         goblinView = new GoblinView(glc);
     }
