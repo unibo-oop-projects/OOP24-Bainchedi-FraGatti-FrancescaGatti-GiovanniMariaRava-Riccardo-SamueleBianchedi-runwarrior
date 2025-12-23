fa3328c44b7fbf630278358c09ebbe5ec14edc41
Danilo Pianini <danilo.pianini@unibo.it>
2025-12-23 17:34:12 +0100

    ci: add the OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
-rootProject.name = "oop-24-runwarrior"
+rootProject.name = "oop-25-runwarrior"
 plugins {
-    id("com.gradle.develocity") version "4.2.2"
+    id("com.gradle.develocity") version "4.3"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
